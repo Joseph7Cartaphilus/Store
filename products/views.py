f5d78e92dd520fb9bf9d480a2db5539bbf327678
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-05 19:52:10 +0300

    add base html and did the DRY

--- products/views.py
+++ products/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    return render(request, 'products/index.html', {
+        'title': 'Store'
+    })
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    return render(request, 'products/products.html',{
+        'title': 'Store - products'
+    })
