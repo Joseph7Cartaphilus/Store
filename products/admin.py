ea282c85ce24edc8a16f872e79a0700d79470c14
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-05 21:24:00 +0300

    make a difference admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProductCategory, Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'short_description', 'price', 'quantity',
+                    'category']  # настройка отображения
+    list_per_page = 5  # Пагинация
+
+
+@admin.register(ProductCategory)
+class ProductCategory(admin.ModelAdmin):
+    list_display = ['name', 'description'] # настройка отображения
+    list_per_page = 5  # Пагинация
+
