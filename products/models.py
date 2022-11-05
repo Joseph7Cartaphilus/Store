be644f28dd292dd9c09545d81b71000c6112810b
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-05 21:24:13 +0300

    fixed model

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'Product Categories'
+
     def __str__(self):
         return self.name
 
@@ -14,6 +17,9 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products_images', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     short_description = models.CharField(max_length=64, blank=True, null=True)
-    price = models.DecimalField(max_digits=2, decimal_places=2, default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'Продукт: "{self.name}" категории: "{self.category}"'
