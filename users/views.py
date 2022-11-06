ed0fc40c4a631bd76cdf93f997a1d116f29d9a80
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-06 20:08:44 +0300

    login form

--- users/views.py
+++ users/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+
+from users.forms import UserLoginForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login.html', {
+        'form': form
+    })
 
 
 def register(request):
