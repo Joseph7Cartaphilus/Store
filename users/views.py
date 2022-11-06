9ac7fa34e6633b19a0564f2952987f99d3d4c67e
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-06 22:19:52 +0300

    add messages in templates

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
@@ -27,6 +27,7 @@ def register(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'You have successfully registered')
             return HttpResponseRedirect(reverse('login'))
     else:
         form = UserRegistrationForm()
